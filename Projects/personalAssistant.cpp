cda1638dcb1935903e01eff93489f9edca279b57
vartikabhawsar <98348633+vartikabhawsar@users.noreply.github.com>
2022-08-20 02:37:50 +0530

    Add files via upload

--- Projects/personalAssistant.cpp
+++ Projects/personalAssistant.cpp
@@ -15,24 +15,24 @@ void greetings(){
     tm *time = localtime(&now);
 
     if (time-> tm_hour < 12){
-        cout<< "Good Morning Master Aditya"<<endl;
-        string phrase = "Good Morning Master Aditya";
+        cout<< "Good Morning vartika"<<endl;
+        string phrase = "Good Morning vartika";
         string command = "espeak \"" + phrase + "\"";
         const char *charCommand = command.c_str();
         system(charCommand);
     }
 
     else if (time-> tm_hour >= 12 && time->tm_hour <= 16){
-        cout<< "Good Afternoon Master Aditya"<<endl;
-        string phrase = "Good Afternoon Master Aditya";
+        cout<< "Good Afternoon vartika"<<endl;
+        string phrase = "Good Afternoon vartika";
         string command = "espeak \"" + phrase + "\"";
         const char *charCommand = command.c_str();
         system(charCommand);
     }
 
     else if (time-> tm_hour > 16 && time->tm_hour < 24){
-        cout<< "Good Evening Master Aditya"<<endl;
-        string phrase = "Good Evening Master Aditya";
+        cout<< "Good Evening vartika"<<endl;
+        string phrase = "Good Evening vartika";
         string command = "espeak \"" + phrase + "\"";
         const char *charCommand = command.c_str();
         system(charCommand);
@@ -75,9 +75,9 @@ int main()
             greetings();
             do{
                 cout<<"\n<**********************************************************************************************>\n\n";
-                cout<<endl<<"How can i help you Master Aditya...."<<endl<<endl;
+                cout<<endl<<"How can i help you vartika...."<<endl<<endl;
 
-                string phrase = "How can i help you Master Aditya";
+                string phrase = "How can i help you vartika";
                 string command = "espeak \"" + phrase + "\"";
                 const char *charCommand = command.c_str();
                 system(charCommand);
@@ -89,8 +89,8 @@ int main()
 
                 if(command=="hello" || command=="hi")
                 {
-                    cout<<"Hello Master Aditya....."<<endl;
-                    string phrase = "Hello Master Aditya";
+                    cout<<"Hello vartika....."<<endl;
+                    string phrase = "Hello vartika";
                     string command = "espeak \"" + phrase + "\"";
                     const char *charCommand = command.c_str();
                     system(charCommand);
@@ -105,23 +105,23 @@ int main()
 
                 else if(command=="who are you" || command=="who is speaking")
                 {
-                    cout<<"I'm a personal assistant created by Master Aditya !!!"<<endl;
-                    string phrase = "I'm a personal assistant created by Master Aditya !!!";
+                    cout<<"I'm a personal assistant created by vartika !!!"<<endl;
+                    string phrase = "I'm a personal assistant created by vartika !!!";
                     string command = "espeak \"" + phrase + "\"";
                     const char *charCommand = command.c_str();
                     system(charCommand);
                 }
 
 
-    //             else if(command=="open notepad")
-    //             {
-    //                 cout<<"openining notepad"<<endl;
-    //                 string phrase = "opening notepad";
-    //                 string command = "espeak \"" + phrase + "\"";
-    //                 const char *charCommand = command.c_str();
-    //                 system(charCommand);
-    //                 CreateProcess(TEXT("C:\\Windows\\notepad.exe"), NULL, NULL, NULL, FALSE, NULL, NULL, NULL, &startInfo, &processInfo);
-    //             }
+                else if(command=="open notepad")
+                {
+                    cout<<"openining notepad"<<endl;
+                    string phrase = "opening notepad";
+                    string command = "espeak \"" + phrase + "\"";
+                    const char *charCommand = command.c_str();
+                    system(charCommand);
+                    CreateProcess(TEXT("C:\\Windows\\notepad.exe"), NULL, NULL, NULL, FALSE, NULL, NULL, NULL, &startInfo, &processInfo);
+                }
 
     //             else if(command=="open writepad")
     //             {
@@ -133,25 +133,25 @@ int main()
     //                 CreateProcess(TEXT("C:\\Windows\\write.exe"), NULL, NULL, NULL, FALSE, NULL, NULL, NULL, &startInfo, &processInfo);
     //             }
 
-    //             else if(command=="open Microsoft Excel")
-    //             {
-    //                 cout<<"opening Microsoft Excel"<<endl;
-    //                 string phrase = "opening Microsoft Excel";
-    //                 string command = "espeak \"" + phrase + "\"";
-    //                 const char *charCommand = command.c_str();
-    //                 system(charCommand);
-    //                 CreateProcess(TEXT("C:\\Program Files\\WindowsApps\\Microsoft.Office.Desktop.Excel_16051.14326.20238.0_x86__8wekyb3d8bbwe\\Office16\\EXCEL.exe"), NULL, NULL, NULL, FALSE, NULL, NULL, NULL, &startInfo, &processInfo);
-    //             }
+                else if(command=="open microsoft excel")
+                {
+                    cout<<"opening Microsoft Excel"<<endl;
+                    string phrase = "opening Microsoft Excel";
+                    string command = "espeak \"" + phrase + "\"";
+                    const char *charCommand = command.c_str();
+                    system(charCommand);
+                    CreateProcess(TEXT("C:\\Program Files\\Microsoft Office\\root\\Office16\\EXCEL.EXE"), NULL, NULL, NULL, FALSE, NULL, NULL, NULL, &startInfo, &processInfo);
+                }
 
-    //             else if(command=="open Microsoft Word")
-    //             {
-    //                 cout<<"opening Microsoft Word"<<endl;
-    //                 string phrase = "opening Microsoft Word";
-    //                 string command = "espeak \"" + phrase + "\"";
-    //                 const char *charCommand = command.c_str();
-    //                 system(charCommand);
-    //                 CreateProcess(TEXT("C:\\Program Files\\Microsoft Office\\root\\Office16\\WINWORD.EXE"), NULL, NULL, NULL, FALSE, NULL, NULL, NULL, &startInfo, &processInfo);
-    //             }
+                else if(command=="open microsoft word")
+                {
+                    cout<<"opening Microsoft Word"<<endl;
+                    string phrase = "opening Microsoft Word";
+                    string command = "espeak \"" + phrase + "\"";
+                    const char *charCommand = command.c_str();
+                    system(charCommand);
+                    CreateProcess(TEXT("C:\\Program Files\\Microsoft Office\\root\\Office16\\WINWORD.EXE"), NULL, NULL, NULL, FALSE, NULL, NULL, NULL, &startInfo, &processInfo);
+                }
 
     //             else if(command=="open Microsoft Paint")
     //             {
@@ -174,46 +174,46 @@ int main()
     //                 mciSendString("play mp3", NULL, 0, NULL);
     //             }
 
-    //             else if(command=="open google")
-    //             {
-    //                 cout<<"openining google"<<endl;
-    //                 string phrase = "opening google";
-    //                 string command = "espeak \"" + phrase + "\"";
-    //                 const char *charCommand = command.c_str();
-    //                 system(charCommand);
-    //                 system("start https://www.google.com");
-    //             }
+                else if(command=="open google")
+                {
+                    cout<<"openining google"<<endl;
+                    string phrase = "opening google";
+                    string command = "espeak \"" + phrase + "\"";
+                    const char *charCommand = command.c_str();
+                    system(charCommand);
+                    system("start https://www.google.com/");
+                }
 
-    //             else if(command=="open youtube")
-    //             {
-    //                 cout<<"openining YouTube"<<endl;
-    //                 string phrase = "opening youtube";
-    //                 string command = "espeak \"" + phrase + "\"";
-    //                 const char *charCommand = command.c_str();
-    //                 system(charCommand);
-    //                 system("start https://www.youtube.com");
-    //             }
+                else if(command=="open youtube")
+                {
+                    cout<<"opening YouTube"<<endl;
+                    string phrase = "opening youtube";
+                    string command = "espeak \"" + phrase + "\"";
+                    const char *charCommand = command.c_str();
+                    system(charCommand);
+                    system("start https://www.youtube.com");
+                }
 
-    //             else if(command=="close chrome browser")
-    //             {
-    //                 cout<<"closing chrome browser"<<endl;
-    //                 string phrase = "closing chrome browser";
-    //                 string command = "espeak \"" + phrase + "\"";
-    //                 const char *charCommand = command.c_str();
-    //                 system(charCommand);
-    //                 system("TASKKILL /IM chrome.exe /F");
-    //             }
+                else if(command=="close chrome browser")
+                {
+                    cout<<"closing chrome browser"<<endl;
+                    string phrase = "closing chrome browser";
+                    string command = "espeak \"" + phrase + "\"";
+                    const char *charCommand = command.c_str();
+                    system(charCommand);
+                    system("TASKKILL /IM chrome.exe /F");
+                }
 
 
-    //             else if(command=="open photo")
-    //             {
-    //                 cout<<"opening photo"<<endl;
-    //                 string phrase = "opening photo";
-    //                 string command = "espeak \"" + phrase + "\"";
-    //                 const char *charCommand = command.c_str();
-    //                 system(charCommand);
-    //                 ShellExecute(NULL,"open","vartika.jpeg",NULL, NULL, SW_NORMAL);
-    //             }
+                else if(command=="open photo")
+                {
+                    cout<<"opening photo"<<endl;
+                    string phrase = "opening photo";
+                    string command = "espeak \"" + phrase + "\"";
+                    const char *charCommand = command.c_str();
+                    system(charCommand);
+                    ShellExecute(NULL,"open","vartika.jpeg",NULL, NULL, SW_NORMAL);
+                }
 
 
     //             else if(command=="open book")
@@ -238,8 +238,8 @@ int main()
 
     //             else if(command=="bye" || command=="exit")
     //             {
-    //                 cout<<"Good Bye Master Aditya, see you soon!"<<endl;
-    //                 string phrase = "Good Bye Master Aditya, see you soon!";
+    //                 cout<<"Good Bye vartika, see you soon!"<<endl;
+    //                 string phrase = "Good Bye vartika, see you soon!";
     //                 string command = "espeak \"" + phrase + "\"";
     //                 const char *charCommand = command.c_str();
     //                 system(charCommand);
